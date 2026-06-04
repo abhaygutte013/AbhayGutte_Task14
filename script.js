c6df3a8425c6da80e8036b5c56125081faf19113
Abhay Gutte <abhaygutte292@gmail.com>
2026-06-04 21:14:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,76 @@
-// User Input
-let n = parseInt(prompt("Enter a number:"));
+let input =prompt("Enter a number;");
+let number = Number(input);
 
-// Sum of first n numbers
-let sumN = 0;
-for (let i = 1; i <= n; i++) {
-    sumN += i;
-}
-
-// Table of n
-console.log("Table of " + n + ":");
-for (let i = 1; i <= 10; i++) {
-    console.log(n + " x " + i + " = " + (n * i));
-}
-
-// Prime Number Check
-let isPrime = true;
-if (n <= 1) {
-    isPrime = false;
-} else {
-    for (let i = 2; i < n; i++) {
-        if (n % i === 0) {
-            isPrime = false;
-            break;
+if (isNaN(number)){
+    console.log("please enter a valid number.");
+} else if(number<0){
+    console.log("Please enter a positive number.");
+}else{
+    function sumOfFirstN(n){
+        let sum=0;
+        for (let i=1;i<=n;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+     function printTable(n){
+        console.log("Multiplication table:");
+        for (let i=1;i<=10; i++){
+            console.log(`${n} x ${i} = ${n *i}`);
         }
     }
-}
-
-// Factors
-let factors = "";
-for (let i = 1; i <= n; i++) {
-    if (n % i === 0) {
-        factors += i + " ";
+    function checkPrime(n){
+        if(n<=1){
+            return false;
+        }
+        for (let i=2; i<n; i++){
+            if(n%i ===0){
+                return false; 
+            }
+        }
+        return true;
     }
+    function getFactors(n){
+        let factors=[];
+        for(let i=1;i<=n;i++){
+            if (n%i===0){
+                factors.push(i);
+            }
+        }
+        return factors;
+    }
+    function sumOfDigits(n){
+        let sum=0;
+        let temp=n;
+
+        while (temp>0){
+            sum+=temp % 10;
+            temp=Math.floor(temp/10);
+        }
+        return sum;
+    }
+    function checkArmstrong(n){
+        let digits=n.toString().length;
+        let temp=n;
+        let total=0;
+        while (temp>0){
+            let digit=temp % 10;
+            total+=digit**digits;
+            temp=Math.floor(temp/10);
+        }
+        return total===n;
+    }
+
+console.log("number:",number);
+console.log("sum of first n numbers:",sumOfFirstN(number));
+printTable(number);
+console.log("prime number:",checkPrime(number)?"yes":"no");
+console.log("factors:",getFactors(number).join(","));
+console.log("sum of digits:",sumOfDigits(number));
+console.log("Armstrong Number:",checkArmstrong(number)?"yes":"no");
 }
 
-// Sum of Digits
-let temp = n;
-let digitSum = 0;
-while (temp > 0) {
-    digitSum += temp % 10;
-    temp = Math.floor(temp / 10);
-}
 
-// Armstrong Number Check
-temp = n;
-let armstrongSum = 0;
-while (temp > 0) {
-    let digit = temp % 10;
-    armstrongSum += digit ** 3;
-    temp = Math.floor(temp / 10);
-}
-let isArmstrong = armstrongSum === n;
 
-// Output
-console.log("Number: " + n);
-console.log("Sum of first " + n + " numbers: " + sumN);
-console.log("Sum of digits: " + digitSum);
-console.log("Is it an Armstrong number? " + (isArmstrong ? "Yes" : "No"));
-console.log("Is it a prime number? " + (isPrime ? "Yes" : "No"));
-console.log("Factors: " + factors);
+
+
